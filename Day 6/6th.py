6a4c13923b7103c70ef2f33c57bd92ff1c6cb147
sudarsancse <sudarsansarkarcst@gmail.com>
2025-04-19 13:55:29 +0530

    Day 6 Python Project

---
+++ Day 6/6th.py
@@ -0,0 +1,19 @@
+#function
+""" def my_function():
+    print("hello")
+    print("World")
+
+my_function() """
+
+
+""" def mul (a , b ):
+    sum = a+b
+    return sum
+
+print(mul(1 ,2)) """
+
+#while loop
+number = 5
+while number > 0 :
+    number -=1
+    print(number)
